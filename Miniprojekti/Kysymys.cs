b70d475cbe5cdb60029197f402dafdde1dfa9d3c
EMILIAS <emilia.streng@gmail.com>
2018-09-19 15:04:00 +0300

    Yhdistetyt pelit 1-2

--- Miniprojekti/Kysymys.cs
+++ Miniprojekti/Kysymys.cs
@@ -1,42 +1,39 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-
+namespace Viikko1_Quizproject
+{
     class Kysymys
     {
-        public string Kysymysteksti { get; set; }
-        public List<string> VastausVaihtoehdot;
-        public int OikeaVaihtoehto;
-        //int kysymysNro;
-  
-        public Kysymys(int arvottuRiviNro, List<string[]> kokoLista)
-        {
-            string[] yksiKysymys = kokoLista[arvottuRiviNro];
-            this.Kysymysteksti = yksiKysymys[0];
-            this.OikeaVaihtoehto = Int32.Parse(yksiKysymys[1]);
-            string[] erotellut = yksiKysymys[2].Split('*');
-            List<string> vaihtoehdot = new List<string>();
-            foreach (string elem in erotellut)
-            {
-                vaihtoehdot.Add(elem);
-            }
-           this.VastausVaihtoehdot = vaihtoehdot;
-        }
-        public override string ToString() {
-            return (Kysymysteksti + "\n\n" + VastausVaihtoehdot[0] + "\n" + VastausVaihtoehdot[1] + "\n" + VastausVaihtoehdot[2] + "\n");
-        }
-        public bool TarkistaVastaus(string kayttajanVastaus) {
-            if (this.OikeaVaihtoehto == int.Parse(kayttajanVastaus)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        public void TulostaKysymys()
-        {
-            Console.WriteLine(Kysymysteksti + "\n\n" + VastausVaihtoehdot[0] + "\n" + VastausVaihtoehdot[1] + "\n" + VastausVaihtoehdot[2] + "\n");
-    }
-    }
+        private string kysymysTeksti;
+        private List<String> vastausVaihtoehdot;
+        private int oikeanVaihtoehdonIndeksi;
+        public string KysymysTeksti { get => kysymysTeksti; set => kysymysTeksti = value; }
+        public int OikeanVaihtoehdonIndeksi { get => oikeanVaihtoehdonIndeksi; set => oikeanVaihtoehdonIndeksi = value; }
 
+
+        public string KysyKysymys()
+        {
+           
+            string[] kysymykset = File.ReadAllLines(@"../../../Kysymykset.txt");
+
+
+            Random r = new Random();
+
+            for (int i = kysymykset.Length; i > 1;)
+            {
+                int k = r.Next(i--);
+                string temp = kysymykset[i];
+                kysymykset[i] = kysymykset[k];
+                kysymykset[k] = temp;
+            }
+
+            return kysymykset[0];
+
+        }
+    }
+}
--- Miniprojekti/Program.cs
+++ Miniprojekti/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using Viikko1_Quizproject;
 
 namespace Miniprojekti
 {
@@ -12,99 +13,38 @@ namespace Miniprojekti
         public Program() {
             this.Pistelaskuri = 0;
         }
+
         public static void Main(string[] args)
         {
-            do {
-                Program ohjelma = new Program();
-                List<string[]> kokoLista = new List<string[]>();
-                try
-                {
-                    using (var lukija = new StreamReader("../../../kysymykset.txt"))
-                    {
-                        String rivi;
-                        while ((rivi = lukija.ReadLine()) != null)
-                        {
-                            kokoLista.Add(rivi.Split(';'));
-                        }
-                    }
-                }
-                catch (FileNotFoundException) {
-                    Console.WriteLine("Tiedostoa ei löytynyt");
-                }
-                catch (Exception) {
-                    Console.WriteLine("Joku muu virhe sattui.");
-                }
+            Console.WriteLine("Mikä peli valitaan? (1-3)");
+            int luku = int.Parse(Console.ReadLine());
 
-                Random randIndex = new Random();
-                List<Kysymys> Oliot = new List<Kysymys>();
-
-                Console.WriteLine("Tervetuloa! Aloita peli painamalla jotakin nappia.");
-                Console.ReadLine();
-                Console.Write("Montako kysymystä haluat? (1-10) ");
-                
-                int kysymystenMaara = int.Parse(Console.ReadLine());
-                while (kysymystenMaara > 10 || kysymystenMaara < 1)
-                {
-                    Console.WriteLine("Epäkelpo arvo. Anna uusi.");
-                    kysymystenMaara = int.Parse(Console.ReadLine()); 
-                }
-
-                ArrayList kysyttyjenIndeksit = new ArrayList();
-                for (int i = 0; i < kysymystenMaara; i++)
-                {
-                    int arvottuRiviNr = randIndex.Next(0, kokoLista.Count - 1);
-                    // Estetään saman kysymyksen tuleminen uudestaan
-                    if (kysyttyjenIndeksit.Contains(arvottuRiviNr)) {
-                        arvottuRiviNr = randIndex.Next(0, kokoLista.Count - 1);
-                    }
-                    kysyttyjenIndeksit.Add(arvottuRiviNr);
-                    //tehdään olio listan alkiosta jonka indeksi on arvottuRivi
-                    Kysymys kysymysOlio = new Kysymys(arvottuRiviNr, kokoLista);
-                    kysyttyjenIndeksit.Add(arvottuRiviNr);
-                    Oliot.Add(kysymysOlio);
-                }
-                kysyttyjenIndeksit.Clear();
-                
-                string viesti = "";
-
-                TulostaKysymyksetJaTarkista(Oliot, ohjelma);
-                if(ohjelma.Pistelaskuri <= (kysymystenMaara/2))
-                {
-                    viesti = "Ei mennyt ihan putkeen!";
-                }
-                else
-                {
-                    viesti = "Tosi hyva!!!";
-                }
-                Console.WriteLine("Peli on nyt ohi. Vastasit oikein {0}/{1} kysymykseen. {2}", ohjelma.Pistelaskuri, kysymystenMaara, viesti);
-
-                kokoLista.Clear();
-                Console.WriteLine("Haluatko pelata uudestaan? k/e?");
-            } while(Console.ReadLine() == "k");
-        }
-
-        static void TulostaKysymyksetJaTarkista(List<Kysymys> kysymysLista, Program ohjelma)
-        {
-            int kysymysNro = 1;
-            foreach (Kysymys kysymys in kysymysLista)
+            if (luku == 1)
             {
-                Console.WriteLine("Kysymys {0}: ", kysymysNro);
-                kysymys.TulostaKysymys();
-                string kayttajanVastaus = Console.ReadLine();
-                int n;
-                var isNumeric = Int32.TryParse(kayttajanVastaus, out n);
-                while (kayttajanVastaus == null || kayttajanVastaus == "" || !isNumeric) {
-                    Console.WriteLine("Syötteessäsi on jokin pielessä. Yritä uudestaan.");
-                    kayttajanVastaus = Console.ReadLine();
-                    isNumeric = int.TryParse(kayttajanVastaus, out n);
-                }
-                if (kysymys.TarkistaVastaus(kayttajanVastaus))
-                {
-                    ohjelma.Pistelaskuri++;
-                }
-                kysymysNro++;
+                Moottori Peli2 = new Moottori();
+                Peli2.KysymysMoottori();
             }
+
+            else if(luku == 2)
+            {
+                Console.WriteLine("Tervetuloa Tietovisaan!");
+                Console.WriteLine("Vastaa annettuihin kysymyksiin K tai E");
+                Console.WriteLine("***********************");
+
+                Kysymysmoottori.LauranPeli();
+            }
+                
+            
+            else
+            Console.WriteLine("Väärä valinta! :((");
+
+                Console.ReadKey();
         }
+
+       
+      
+
+       
 
     }
 }
